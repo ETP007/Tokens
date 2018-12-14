456e38eb852d3e5508b7e9c9661a42f8b8e4df74
ETP007 <45840377+ETP007@users.noreply.github.com>
2018-12-14 13:47:11 +0800

    Create IEP20.sol

---
+++ IEP20.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.24;
+
+interface IERC20 {
+    function totalSupply()constant returns (uint256 totalSupply);
+    function balanceOf(address _owner) constant returns (uint256 balance);
+    function transfer(address _to, uint256 _value) returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+    function approve(address _spender, uint256 _value) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
